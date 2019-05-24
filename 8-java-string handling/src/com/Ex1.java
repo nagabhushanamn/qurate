a96a9d8ace5dd972734a29d524bd803c26f2e436
nag-training <nag-training@172.20.10.3>
2019-05-24 10:00:31 +0530

    string handling

---
+++ 8-java-string handling/src/com/Ex1.java
@@ -0,0 +1,34 @@
+package com;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+
+		// --------------------------------------
+
+//		String s1="sapient";
+//		String s2=new String("sapient");
+//		
+
+		// --------------------------------------
+
+//		String s1 = "hello";
+//		String s2 = "hello";
+//		System.out.println(s1 == s2);
+//
+//		String s3 = new String("hello");
+//		String s4 = new String("hello");
+//		System.out.println(s3 == s4);
+
+//		--------------------------------------
+
+//		String s = "sapient";
+//		String ss = s.concat("-bengaluru");
+//		System.out.println(s);
+//		System.out.println(ss);
+
+//		--------------------------------------
+
+	}
+
+}
---
+++ 8-java-string handling/src/com/Ex2.java
@@ -0,0 +1,61 @@
+package com;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+
+		String s = "abcdefgha";
+
+		System.out.println(s.charAt(0));
+		System.out.println(s.indexOf('b'));
+		System.out.println(s.indexOf("de"));
+		System.out.println(s.indexOf('a', 3));
+		System.out.println(s.lastIndexOf('a'));
+
+		System.out.println(s.startsWith("a"));
+		System.out.println(s.endsWith("ha"));
+		System.out.println(s.contains("def"));
+
+		System.out.println(s.substring(4));
+		System.out.println(s.substring(4, 6));
+
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+
+		String s1 = "Abc";
+		String s2 = "abc";
+
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+
+		System.out.println(s1.compareTo(s2));
+
+		String s3 = "   hello    ";
+		System.out.println(s3);
+		System.out.println(s3.trim());
+
+		String mobileString = "123456890";
+		String regex = "\\d{10}";
+		System.out.println(mobileString.matches(regex));
+
+		String fridayMenu = "biryani1,biryani2,biryani3";
+		String[] fridayMenuItems = fridayMenu.split(",");
+		for (String item : fridayMenuItems) {
+			System.out.println(item);
+		}
+
+		System.out.println(s.length());
+		System.out.println(s.isEmpty());
+
+		String out = String.format("then sume is %05d", 124);
+		System.out.println(out);
+
+		String s5 = "abc";
+		System.out.println(s5.replace('a', 'A'));
+
+		int n = 123;
+		System.out.println(n);
+
+	}
+
+}
